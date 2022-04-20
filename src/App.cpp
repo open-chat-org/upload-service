88b5bf5ab84de9db4dfd774d9e888974122ada3d
Nguyen Hoang Nam <nguyenhoangnam.dev@gmail.com>
2022-04-20 21:42:42 +0700

    🚧 Upload file and save file with OpenCV

--- src/App.cpp
+++ src/App.cpp
@@ -1,6 +1,20 @@
+#include "App.h"
+
+#include <httpserver.hpp>
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-    std::cout << "Hello World!";
+using namespace httpserver;
+
+void custom_access_log(const std::string &url) {
+    std::cout << "ACCESSING: " << url << std::endl;
+}
+
+int main(int argc, char **argv) {
+    webserver ws = create_webserver(8083).log_access(custom_access_log);
+
+    upload_avatar uploadAvatar;
+    ws.register_resource("/api/v1.0/upload/{user_id}", &uploadAvatar);
+    ws.start(true);
+
     return 0;
 }
